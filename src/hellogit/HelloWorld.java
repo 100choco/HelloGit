189e35ad21d0d0329d7a123e8fc8cc9ee463e877
baeksy <se01163@naver.com>
2021-08-26 09:45:13 +0900

    두번째 라인 추가

--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -3,7 +3,8 @@ package hellogit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-System.out.println("Hellow World");
+		System.out.println("Hellow World");
+		System.out.println("지금 git 수업 중");
 	}
 
 }
