b1b7eaa20673506173dcb0cf242d3081d50473c0
baeksy <se01163@naver.com>
2021-08-26 09:49:01 +0900

    다시 한번 로컬리파지토리에 커밋~~!!!

--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
 		System.out.println("지금 git 수업 중");
+		System.out.println("집중 하세요~~~ 단 한번만 수업합니다.");
 	}
 
 }
